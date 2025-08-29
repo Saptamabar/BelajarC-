c399e280234840ab04b03210ffb112106d96228b
sapta <saptamabar@gmail.com>
2025-08-29 09:48:16 +0700

    Add Soal 3

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿//Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
 FightSystem.start();
 Console.WriteLine(FightSystem.status);
 
-Hero miya = new Hero("Miya",10,90,400);
+Hero miya = new Hero("Miya", 10, 90, 400);
 
-Hero alucard = new Hero("Alucard",9,60,700);
+Hero alucard = new Hero("Alucard", 9, 60, 700);
 alucard.nama = "Alucard";
 alucard.attack_power = 60;
 alucard.health_point = 700;
@@ -86,9 +86,62 @@ class FightSystem
         Console.WriteLine("Pertandingan dimulai");
         status = "Dimulai";
     }
-    public static void stop() 
+    public static void stop()
     {
         Console.WriteLine("Pertandingan Selesai");
         status = "Selesai";
-     }
+    }
+}
+
+//Soal 3
+
+//Buatlah dua namespace: Perpustakaan dan Manajemen
+//Di dalam namespace Perpustakaan, buat kelas Buku dengan:
+//Atribut: string judul, string penulis, int tahunTerbit Konstruktor untuk menginisialisasi atribut dengan this.
+//Method TampilkanInfo() untuk menampilkan informasi buku.Di dalam namespace Manajemen, buat kelas PerpusManager dengan:
+//Method static HitungDenda(int hariTelat) yang menghitung denda berdasarkan hari keterlambatan(misalnya, 1000 per hari).
+
+
+using Manajemen;
+using Perpustakan;
+
+Buku buku = new Buku("Buku","Mas Raka", 2025);
+buku.TampilkanInfo();
+
+PerpusManager.Hitungdenda(10);
+
+namespace Perpustakan
+{
+    class Buku
+    {
+        public string Judul;
+        public string Penulis;
+        public int TahunTerbit;
+        public Buku(string Judul,string Penulis, int TahunTerbit)
+        {
+            this.Judul = Judul;
+            this.Penulis = Penulis;
+            this.TahunTerbit = TahunTerbit;
+        }
+
+        public void TampilkanInfo()
+        {
+            Console.WriteLine($"Judul : {this.Judul}");
+            Console.WriteLine($"Penulis : {this.Penulis}");
+            Console.WriteLine($"Tahun Terbit : {this.TahunTerbit}");
+        }
+    }
+}
+
+namespace Manajemen
+{
+    class PerpusManager
+    {
+        public static void Hitungdenda(int hariterlambat)
+        {
+            int denda = hariterlambat * 1000;
+            Console.WriteLine($"Total denda : {denda}");
+        }
+    }
+
 }
